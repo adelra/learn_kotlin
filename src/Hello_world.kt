69e1499b93e7af60f50dfad9a3bf61833bfb7321
Adel Rahimi <rahimi.adel@gmail.com>
2019-01-18 00:28:50 +0330

    added when expression and edited helloworld

---
+++ src/Hello_world.kt
@@ -0,0 +1,5 @@
+// printing in Kotlin is more or less like Python, instead of the keyword print you have to use println
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+}
---
+++ src/When_expression.kt
@@ -0,0 +1,5 @@
+// When expression in Kotlin is like the case expression in VB or if/elif in Python
+
+fun main(args: Array<String>){
+    when (args[0]=)
+}
