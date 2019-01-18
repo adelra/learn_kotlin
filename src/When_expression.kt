a1a5579990b64a06058c9c6f3a969f13ce1ffbb4
Adel Rahimi <rahimi.adel@gmail.com>
2019-01-18 22:05:39 +0330

    added when expression

--- src/When_expression.kt
+++ src/When_expression.kt
@@ -1,5 +1,7 @@
 // When expression in Kotlin is like the case expression in VB or if/elif in Python
 
-fun main(args: Array<String>){
-    when (args[0]=)
+fun main(args: Array<String>) {
+//    when (args[0] >= 5) {
+//        println("name larger than five characters")
+//    }
 }
